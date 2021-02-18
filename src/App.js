28d400704d6a0bc6be042e01d8b589b783bcdcfa
Monil Narang <monil.narang@olx.com>
2021-02-18 17:08:16 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,118 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import AddTask from './components/AddTask'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
 
-  const onClick = () => {
-    console.log('clickclick')
+  const [showAddTask, setShowAddtask] = useState(false)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
   }
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Category Script',
-        day: 'Feb 9th at 1:30 pm',
-        reminder : true,
-    },
-    {
-        id: 2,
-        text: 'Jmeter',
-        day: 'Feb 6th at 1:30 pm',
-        reminder : true,
-    },
-    {
-        id: 3,
-        text: 'PDP',
-        day: 'Feb 10th at 2:30 am',
-        reminder : true,
-    }
-  ])
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
 
-  const deteleTask = (id) => {
+    return data
+  }
+
+  const deteleTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
     setTasks(tasks.filter((task)=>task.id !== id))
   }
 
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) =>
+     task.id === id ? { ...task, reminder:
+     !task.reminder } : task)
+    )
+  }
+
+  const onClick = () => {
+    setShowAddtask(!showAddTask)
+
+  }  
+
+  const addTask = async (task) => {
+    const res =  await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  }
+
   return (
+    <Router>
     <div className="container">
-      <Header title = 'To-Do List' button = 'Add' onClick = {onClick}/>
-      <Tasks tasks = {tasks} onDelete = {deteleTask} />
+      <Header title = 'To-Do List' onClick = {onClick} showAdd = {showAddTask}/>
+
+      
+      <Route path='/' exact render={(props)=> (
+        <>
+          {showAddTask && <AddTask onAdd = {addTask}/>}
+          
+          {tasks.length>0 ? ( 
+          
+          <Tasks tasks = {tasks}
+            onDelete = {deteleTask}
+            onToggle = {toggleReminder}/>
+            )   :  ('no more') 
+          }
+      
+        </>
+      )}/>
+      <Route path='/about' component={About} />
+    <Footer />
     </div>
-  )
+    
+    </Router>
+  ) 
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import {useLocation} from 'react-router-dom'
 
 
 const Header = (props) => {
+    const location = useLocation()
     return (
             <header className = 'header'>
                 <h1 >{props.title}</h1>
-                <Button text = {props.button} color = 'red'/>
+                {location.pathname === '/' && <Button 
+                 text={props.showAdd ? 'Close' : 'Add'}
+                 color={props.showAdd ? 'red' : 'green'}
+                 onClick={props.onClick} />}
             </header>
     )
 }
